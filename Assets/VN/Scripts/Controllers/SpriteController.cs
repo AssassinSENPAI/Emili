8503b5efa190d2dbe039af326ce275a4bf794bbc
AssassinSeภթสi先輩 <error.kitten.7.7@gmail.com>
2025-03-17 15:02:32 -0400

    Tried to edit SpriteController, failed terribly, added biscuit

--- Assets/VN/Scripts/Controllers/SpriteController.cs
+++ Assets/VN/Scripts/Controllers/SpriteController.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class SpriteController : MonoBehaviour
 {
-    
-}
+    // at the start of the game, every sprite is hidden
+    // main character Emili is always shown at the left of the screen and the other characters are shown at the right of the screen
+    // When the right character is speaking, the sprite will be shown
+    // When another character speaks, the sprite will change to the new character
+    // I can choose to hide the sprite whenever I want
+    // Every sprite has a hide and show animation
+    // And every sprite has different animations for different emotions
+    // there can only be two sprites shown at the same time, Emili and the other character
+
+
+
+    }
